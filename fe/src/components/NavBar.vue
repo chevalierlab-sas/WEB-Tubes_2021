50c7e3eec6ab489305eb921ad6b77cc31f00491d
Rama Sanjaya <ramasanjaya2202@gmail.com>
2022-08-18 23:16:24 +0700

    adding homepage

---
+++ fe/src/components/NavBar.vue
@@ -0,0 +1,86 @@
+<template>
+  <div>
+    <div class="navbar bg-base-100 px-24">
+        <div class="flex-1">
+    <a class="btn btn-ghost normal-case text-xl">
+      <img src="../assets/logo.svg" alt="logo" class="rounded-full p-1">
+      ChevaMovie</a>
+  </div>
+
+      <div class="flex-none">
+        <p class="font-medium">Watchlist</p>
+        <div class="dropdown dropdown-end">
+          <label tabindex="0" class="btn btn-ghost btn-circle">
+            
+            <div class="indicator">
+              <lord-icon
+    src="https://cdn.lordicon.com/ooflgrcu.json"
+    trigger="hover"
+    colors="primary:#e88c30"
+    state="hover-1"
+    style="width:24px;height:24px">
+</lord-icon>
+              <span class="badge badge-sm indicator-item">8</span>
+            </div>    
+          </label>
+          
+          <div
+            tabindex="0"
+            class="mt-3 card card-compact dropdown-content w-52 bg-base-100 shadow"
+          >
+            <div class="card-body">
+              <span class="font-bold text-lg">8 Items</span>
+              <span class="text-info">Subtotal: $999</span>
+              <div class="card-actions">
+                <button class="btn btn-primary btn-block">View cart</button>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="dropdown dropdown-end">
+          <label tabindex="0" class="btn btn-ghost btn-circle avatar">
+            <div class="w-10 rounded-full">
+              <img src="https://placeimg.com/80/80/people" />
+            </div>
+          </label>
+          <ul
+            tabindex="0"
+            class="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
+          >
+            <li>
+              <a class="justify-between">
+                Profile
+                <span class="badge">New</span>
+              </a>
+            </li>
+            <li><a>Settings</a></li>
+            <li><a>Logout</a></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: "NavBar",
+  data() {
+    return {
+      isLogin: false,
+      user: {},
+    };
+  },
+  created() {},
+  methods: {
+    doLogout() {
+      // getAuth().signOut()
+      // .then(() => {
+      //   window.location.reload();
+      // })
+      // .catch((err) => alert(err.message));
+    },
+  },
+};
+</script>
---
+++ fe/src/views/Home.vue
@@ -0,0 +1,65 @@
+<template>
+  <div>
+    <!-- hero -->
+    <div class="hero min-h-screen bg-base-200">
+  <div class="hero-content flex-col lg:flex-row-reverse">
+    <img src="https://placeimg.com/260/400/arch" class="max-w-lg rounded-lg shadow-2xl" />
+    <div>
+      <h1 class="text-4xl font-bold"><span class="text-orange-400">Cari Film Favoritmu </span> di ChevaMovie</h1>
+      <p class="py-6"> a short description or comment on a movie that is displayed on movie posters (or direct to video covers, etc.) to capture the essence of the movie, and ultimately make you watch the movie. -IMDB</p>
+      <button class="btn btn-primary">Get Started</button>
+    </div>
+  </div>
+</div>
+
+  <!-- Carousel -->
+<div class="w-full px-40 bg-base-300">
+  <div class="carousel">
+  <div id="item1" class="carousel-item w-full">
+    <img src="https://placeimg.com/800/200/arch" class="w-full" />
+  </div> 
+  <div id="item2" class="carousel-item w-full">
+    <img src="https://placeimg.com/800/200/arch" class="w-full" />
+  </div> 
+  <div id="item3" class="carousel-item w-full">
+    <img src="https://placeimg.com/800/200/arch" class="w-full" />
+  </div> 
+  <div id="item4" class="carousel-item w-full">
+    <img src="https://placeimg.com/800/200/arch" class="w-full" />
+  </div>
+</div> 
+<div class="flex justify-center py-2 gap-2">
+  <a href="#item1" class="btn btn-xs">1</a> 
+  <a href="#item2" class="btn btn-xs">2</a> 
+  <a href="#item3" class="btn btn-xs">3</a> 
+  <a href="#item4" class="btn btn-xs">4</a>
+</div>
+</div>
+  <!-- card -->
+  <div>
+    <h1>Film Terpopuler</h1>
+        <app-list>
+            <movie-card></movie-card>
+            <movie-card></movie-card>
+            <movie-card></movie-card>
+            <movie-card></movie-card>
+            <movie-card></movie-card>
+            <movie-card></movie-card>
+        </app-list>
+  </div>
+
+  </div>
+</template>
+
+<script>
+import MovieCard from "../components/MovieCard.vue"
+import ListVue from '../components/List.vue';
+
+export default{
+    name: 'Home',
+    components: {
+      MovieCard,
+      'app-list' : ListVue,
+    },
+}
+</script>
