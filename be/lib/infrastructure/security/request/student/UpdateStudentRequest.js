208b170d7271235c2a5d446109c5ee596b7f12e4
mrzf833 <palaktais@gmail.com>
2022-08-23 11:06:01 +0700

    add student request

---
+++ be/lib/infrastructure/security/request/student/UpdateStudentRequest.js
@@ -0,0 +1,73 @@
+const { checkSchema }  = require('express-validator')
+const CheckValidation = require('../../CheckValidation')
+const StudentModel = require('../../../database/orm/sequilize/models/Student')
+
+module.exports = [
+    checkSchema({
+        fullname: {
+            in: ['body'],
+            notEmpty: {
+                errorMessage: "fullname is required"
+            },
+            isLength: {
+                options: {
+                    min: 1,
+                    max: 225
+                },
+                errorMessage: "name length min 1, max 225"
+            }
+        },
+        email: {
+            in: ['body'],
+            notEmpty: {
+                errorMessage: "email is required"
+            },
+            isLength: {
+                options: {
+                    min: 1,
+                    max: 225
+                },
+                errorMessage: "email length min 1, max 225"
+            },
+            isEmail: true,
+            custom: {
+                options: async (value, {req}) => {
+                    const count = await StudentModel.count({
+                        distinct: true,
+                        col: 'id',
+                        where: {
+                            email: value,
+                            id: req.params.studentId
+                        }
+                    })
+
+                    if (count){
+                        throw new Error('E-mail already in use')
+                    }
+
+                    return count
+                }
+            }
+        },
+        phone_number: {
+            in: ['body'],
+            notEmpty: {
+                errorMessage: "phone_number is required"
+            },
+            isLength: {
+                options: {
+                    min: 1,
+                    max: 15
+                },
+                errorMessage: "phone_number length min 1, max 15"
+            },
+            isNumeric: true
+        },
+        address: {
+            in: ['body'],
+            optional: true,
+            isString: true
+        }
+    }),
+    CheckValidation
+]
