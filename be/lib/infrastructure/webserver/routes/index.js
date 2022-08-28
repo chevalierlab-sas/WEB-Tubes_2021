ea9cb0221c94b65955d14fdd8345015a1bb4293c
mrzf833 <palaktais@gmail.com>
2022-08-28 15:30:12 +0700

    add role in user

--- be/lib/infrastructure/webserver/routes/index.js
+++ be/lib/infrastructure/webserver/routes/index.js
@@ -6,7 +6,7 @@ const SuperAdmin = require('../../security/SuperAdmin')
 router.use('/inventory', auth, AdminOrSuperAdmin, require('./inventory'))
 router.use('/category', auth, AdminOrSuperAdmin, require('./category'))
 router.use('/student', auth, AdminOrSuperAdmin, require('./student'))
-router.use('/user', require('./user'))
+router.use('/user', auth, SuperAdmin, require('./user'))
 router.use('/', require('./auth'))
 
 module.exports = router
